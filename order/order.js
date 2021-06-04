02636165bfca722d04c2796e5a118db8a1b703c5
antirusuh <rizal.sufacete@gmail.com>
2021-06-04 09:45:54 +0700

    Upload Pertama

---
+++ order/order.js
@@ -0,0 +1,486 @@
+let rawData = [{
+    id : 'budi707',
+    namaPengirim : 'Budi',
+    noHpPengirim : '+623618754707',
+    alamatPengirim : `Gang XV, No. 21, Bali`,
+    kotaAsal : `Bali`,
+    namaPenerima : 'Indah Utami Hartono',
+    noHpPenerima : '+62 21 37003136',
+    alamatPenerima : `Ruko Kranggan Permai Blok Rt. 17 No. 28, Jati Sampurna, Jawa Barat`,
+    kotaTujuan : 'Bekasi',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Benny350',
+    namaPengirim : 'Benny Hartono Sasmita',
+    noHpPengirim : '0-21-521-3350',
+    alamatPengirim : `Jl HR Rasuna Said Kav C-6 Wisma Budi Lt 5 Suite 503`,
+    kotaAsal : `Jakarta`,
+    namaPenerima : 'Cinta Nirmala Yuwono',
+    noHpPenerima : '+880-31-726224',
+    alamatPenerima : `Jl. Sudirman no 10, kel. depok, kec. depok`,
+    kotaTujuan : 'bandung',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Cahya961',
+    namaPengirim : 'Cahya Citra Darmadi',
+    noHpPengirim : '+88029134961',
+    alamatPengirim : `822/4 Begum Rokeya Sarani`,
+    kotaAsal : `Pontianak`,
+    namaPenerima : 'Sri Fanny Muljana',
+    noHpPenerima : '024-55668874',
+    alamatPenerima : `james street, papua,  02DF5`,
+    kotaTujuan : 'Papua',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Cinta224',
+    namaPengirim : 'Cinta Nirmala Yuwono',
+    noHpPengirim : '+880-31-726224',
+    alamatPengirim : `Jl. Sudirman no 10, kel. depok, kec. depok`,
+    kotaAsal : `Medan`,
+    namaPenerima : 'Cahaya Fanny Darmadi',
+    noHpPenerima : '081222221299',
+    alamatPenerima : `Prambanan 3 Blok H1 N0. 23, Jawa Barat`,
+    kotaTujuan : 'Tegal',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Tanu462',
+    namaPengirim : 'Tanudisastro Bo Hermanto',
+    noHpPengirim : '0-61-452-5462',
+    alamatPengirim : `Jl Orion 85, Sumatera Utara`,
+    kotaAsal : `Banda Aceh`,
+    namaPenerima : 'Cinta Susanti Setiawan',
+    noHpPenerima : '021 5824346',
+    alamatPenerima : `Jl Puri Kencana Rukan Puri Kencana II Bl J-1/1-AB`,
+    kotaTujuan : 'Tasikmalaya',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Bima327',
+    namaPengirim : 'Bima Agung Sutedja',
+    noHpPengirim : '+880-31-722327',
+    alamatPengirim : `Haroon Bhaban, (ground Floor)`,
+    kotaAsal : `Garut`,
+    namaPenerima : 'Siska Batari Atmadjaja',
+    noHpPenerima : '(031) 891 0547',
+    alamatPenerima : `Jalan Sukodono Keboan Anom, Gedangan Sidoarjo`,
+    kotaTujuan : 'Tasikmalaya',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Indah251',
+    namaPengirim : 'Indah Sari Rachman',
+    noHpPengirim : '021 42879251',
+    alamatPengirim : `Jl Cempaka Putih Raya 3,Cempaka Putih Timur`,
+    kotaAsal : `Banda Aceh`,
+    namaPenerima : 'Leony Batari Widjaja',
+    noHpPenerima : '0-216-232-0227',
+    alamatPenerima : `LTC Glodok GF1 Blok C8 no.5`,
+    kotaTujuan : 'Jogja',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Hart070',
+    namaPengirim : 'Hartanti Dwi Agusalim',
+    noHpPengirim : '0285 421070',
+    alamatPengirim : `Jl Poncol III 21`,
+    kotaAsal : `Jogja`,
+    namaPenerima : 'Lestari Vera Widjaja',
+    noHpPenerima : '021 5363055',
+    alamatPenerima : `Jl Letjen S Parman Kav 77`,
+    kotaTujuan : 'Cirebon',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Sudi423',
+    namaPengirim : 'Sudirman Handoko Atmadjaja',
+    noHpPengirim : '021-3162423',
+    alamatPengirim : `Jl Salemba Tgh 39-BB 10440`,
+    kotaAsal : `Cirebon`,
+    namaPenerima : 'Cahaya Utami Gunawan',
+    noHpPenerima : '021 2521389',
+    alamatPenerima : `Jl Jend Sudirman Kav 29-31 Wisma Metropolitan I Lt 3`,
+    kotaTujuan : 'Makasar',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Inge340',
+    namaPengirim : 'Inge Yuliani Kurniawan',
+    noHpPengirim : '021-5659340',
+    alamatPengirim : `JL Empang Bahagia Raya No. 2-A RT 001/0611460`,
+    kotaAsal : `Cirebon`,
+    namaPenerima : 'Ary Tri Santoso',
+    noHpPenerima : '021 6019438',
+    alamatPenerima : `Jl Mangga Dua Raya Ruko Tekstil Mangga Dua Bl C-3/12`,
+    kotaTujuan : 'Bandar Lampung',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Utar217',
+    namaPengirim : 'Utari Ida Oesman',
+    noHpPengirim : '0-21-461-9217',
+    alamatPengirim : `Jl Raya Bekasi Km 20 RT 001/13`,
+    kotaAsal : `Serang`,
+    namaPenerima : 'Shinta Sucianty Pranoto',
+    noHpPenerima : '021 63858570',
+    alamatPenerima : `Jl Suryopranoto 2 Harmoni Plaza Bl E/20`,
+    kotaTujuan : 'Jakarta',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Ades345',
+    namaPengirim : 'Ade Sari Budiaman',
+    noHpPengirim : '+880-2-8117345',
+    alamatPengirim : `Jl. Sudirman no 15, kel. depok, kec. badung`,
+    kotaAsal : `Tasikmalaya`,
+    namaPenerima : 'Cahaya Fanny Rachman',
+    noHpPenerima : '021-95713291',
+    alamatPenerima : `Jl. Wijaya VIII No. 2 Melawai`,
+    kotaTujuan : 'Surakarta',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Ekow471',
+    namaPengirim : 'Eko Widya Sasmita',
+    noHpPengirim : '0-21-921-7471',
+    alamatPengirim : `Jl Prapanca Buntu 14 C`,
+    kotaAsal : `Depok`,
+    namaPenerima : 'Adi Wibawa Irawan',
+    noHpPenerima : '021-3142882',
+    alamatPenerima : `JL.Pentaran No.9 Proklamasi`,
+    kotaTujuan : 'Medan',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Djaj442',
+    namaPengirim : 'Djaja Susila Hadiman',
+    noHpPengirim : '021 8580442',
+    alamatPengirim : `Jl Utan Bahagia Raya Ruko Utan Kayu Lt 3/70-B`,
+    kotaAsal : `Tegal`,
+    namaPenerima : 'Sinta Sari Pranoto',
+    noHpPenerima : '021 46827934',
+    alamatPenerima : `Jl Raya Cakung Cilincing Km 3`,
+    kotaTujuan : 'Bogor',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Aris082',
+    namaPengirim : 'Ari Suhendra Budiaman',
+    noHpPengirim : '021 6314082',
+    alamatPengirim : `Jl Suryopranoto 2 Kompl Harmoni Plaza Bl E/18-19`,
+    kotaAsal : `Tasikmalaya`,
+    namaPenerima : 'Suharto Ari Sutedja',
+    noHpPenerima : '+880-2-9885776',
+    alamatPenerima : `House # 307, Lane # 21`,
+    kotaTujuan : 'Serang',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}, {
+    id : 'Adeh733',
+    namaPengirim : 'Ade Herman Setiawan',
+    noHpPengirim : '+880-31-619733',
+    alamatPengirim : `H.s.s. Road 10`,
+    kotaAsal : `Medan`,
+    namaPenerima : 'Glenna Sinta Sutedja',
+    noHpPenerima : '085721127793',
+    alamatPenerima : `jalan Panyawungan no. 48 rt. 07/03 cileunyi wetan`,
+    kotaTujuan : 'Palembang',
+    biaya : 10000,
+    status: `Sedang Dalam Pengiriman`,
+    updated : false
+}]
+    
+let kamusKota = ['Banda Aceh'
+    , 'Medan',
+    'Palembang',
+    'Bandar Lampung',
+    'Serang',
+    'Jakarta',
+    'Bogor',
+    'Depok',
+    'Bekasi',
+    'Tangerang',
+    'Bandung',
+    'Tasikmalaya',
+    'Garut',
+    'Cirebon',
+    'Indramayu',
+    'Tegal',
+    'Jogja',
+    'Surakarta',
+    'Denpasar',
+    'Makasar',
+    'Lombok']
+
+// function hapusData(params) {
+//     alert(`hapus`)
+//     for (let i = 0; i < params.length; i++) {
+//         // console.log(params[i]);
+//         if (params[i].id === 'budi123') {
+//             delete params[i]
+//         }
+//     }
+//     console.log(params);
+// }
+
+let array = [{
+    id : 'budi123',
+    namaPengirim : 'Budi',
+    noHpPengirim : '081235221123',
+    alamatPengirim : `Jl. Sudirman no 10, kel. depok, kec. depok`,
+    kotaAsal : `Jakarta`,
+    namaPenerima : 'Asep',
+    noHpPenerima : '081222221299',
+    alamatPenerima : `Jl. Jatinangor no 13, kel. Bojongsoang, kec. Cikaso`,
+    kotaTujuan : 'Bandung',
+    biaya : 10000,
+    status : `Sedang Dalam Pengiriman`,
+    updated : false
+}]
+
+function generateTable(data) {
+    let database = document.getElementById('tabelBody');
+    database.innerHTML = ''
+
+    for (let i = 0; i < data.length; i++) { // array.length
+        let tdNum = document.createElement ('td')
+        let tdId = document.createElement ('td')
+        let tdNamaPengirim = document.createElement ('td')
+        let tdNoPengirim = document.createElement ('td')
+        let tdAlamatPengirim = document.createElement ('td')
+        let tdKotaAsal = document.createElement ('td')
+        let tdNamaPenerima = document.createElement ('td')
+        let tdNoPenerima = document.createElement ('td')
+        let tdAlamatPenerima = document.createElement ('td')
+        let tdKotaTujuan = document.createElement ('td')
+        let tdBiaya = document.createElement ('td')
+        let tdStatus = document.createElement ('td')
+        let tdButton = document.createElement('button')
+        // let tdButtonUp = document.createElement('button') // => create button update
+        let isiHasil = data[i] // array
+        let createTr = document.createElement ('tr')
+
+        tdNum.innerHTML = i+1
+        tdId.innerHTML = isiHasil.id
+        tdNamaPengirim.innerHTML = isiHasil.namaPengirim
+        tdNoPengirim.innerHTML = isiHasil.noHpPengirim
+        tdAlamatPengirim.innerHTML = isiHasil.alamatPengirim
+        tdKotaAsal.innerHTML = isiHasil.kotaAsal
+        tdNamaPenerima.innerHTML = isiHasil.namaPenerima
+        tdNoPenerima.innerHTML = isiHasil.noHpPenerima
+        tdAlamatPenerima.innerHTML = isiHasil.alamatPenerima
+        tdKotaTujuan.innerHTML = isiHasil.kotaTujuan
+        tdBiaya.innerHTML = isiHasil.biaya
+        tdStatus.innerHTML = isiHasil.status
+        tdButton.innerHTML = "Delete";
+        // tdButtonUp.innerHTML = "Update"; // => inner.HTML Update
+
+        tdButton.setAttribute('id_paket',i)
+        tdButton.addEventListener("click", deletePaket)
+        
+        // tdButtonUp.setAttribute('id_update',i) // => attribut update
+        // tdButtonUp.addEventListener("click", updatePaket); // => eventlistener update
+        
+        createTr.appendChild (tdNum)
+        createTr.appendChild (tdId)
+        createTr.appendChild (tdNamaPengirim)
+        createTr.appendChild (tdNoPengirim)
+        createTr.appendChild (tdAlamatPengirim)
+        createTr.appendChild (tdKotaAsal)
+        createTr.appendChild (tdNamaPenerima)
+        createTr.appendChild (tdNoPenerima)
+        createTr.appendChild (tdAlamatPenerima)
+        createTr.appendChild (tdKotaTujuan)
+        createTr.appendChild (tdBiaya)
+        createTr.appendChild (tdStatus)
+        // createTr.appendChild (tdButtonUp) // => appendchild update
+        createTr.appendChild (tdButton)
+        database.appendChild(createTr)
+        
+        if (data[i].updated === false) {
+            let tdButtonUp = document.createElement('button') // => create button update
+        
+            tdButtonUp.innerHTML = "Berhasil"; // => inner.HTML Update
+            tdButtonUp.setAttribute('id_berhasil',i) // => attribut update
+            tdButtonUp.addEventListener("click", updateBerhasil); // => eventlistener update
+       
+            createTr.appendChild(tdButtonUp) // => appendchild update
+
+            let tdButtonDown = document.createElement('button') // => create button update
+        
+            tdButtonDown.innerHTML = "Gagal"; // => inner.HTML Update
+            tdButtonDown.setAttribute('id_gagal',i) // => attribut update
+            tdButtonDown.addEventListener("click", updateGagal); // => eventlistener update
+       
+            createTr.appendChild(tdButtonDown) // => appendchild update
+            
+        }
+
+        // var tombolDelete = document.getElementById("delete");
+        // x.addEventListener("click", hapusData(params));
+        
+        // tdButton.addEventListener("click", function () {
+        //     alert("delete is clicked");
+        // });
+        // tdButtonUp.addEventListener("click", function () {
+        //     alert("update is clicked");
+        // });
+
+    }
+}
+
+function deletePaket(event) {
+    // console.log(event.target.getAttribute('id_paket'));
+    const index = Number(event.target.getAttribute('id_paket'))
+    // console.log(index);
+    array.splice(index, 1)
+    
+    generateTable(array)
+}
+
+function tambahPaket() {
+    let tambah = rawData[Math.floor(Math.random() * rawData.length)]
+    array.push(tambah)
+
+    generateTable(array)
+    // console.log('tambah paket');
+}
+
+function updateBerhasil(event) {
+    // console.log(event.target.getAttribute('id_berhasil'));
+    const index = Number(event.target.getAttribute('id_berhasil'))
+
+    // console.log(index);
+    
+    // array[index].status = 'Paket Berhasil dikirim'
+    
+    // let chance = Math.ceil(Math.random() * 100)
+    
+    // if (chance > 50) {
+    //     array[index].status = 'Paket Gagal dikirim'
+    // }
+    // else {
+    // }
+    
+    array[index].status = 'Paket Berhasil dikirim'
+    array[index].updated = true
+
+    generateTable(array)
+}
+
+function updateGagal(event) {
+    // console.log(event.target.getAttribute('id_berhasil'));
+    const index = Number(event.target.getAttribute('id_gagal'))
+
+    // console.log(index);
+    
+    // array[index].status = 'Paket Berhasil dikirim'
+    
+    // let chance = Math.ceil(Math.random() * 100)
+    
+    // if (chance > 50) {
+    // }
+    // else {
+    //     array[index].status = 'Paket Berhasil dikirim'
+    // }
+    
+    array[index].status = 'Paket Gagal dikirim'
+    array[index].updated = true
+
+    generateTable(array)
+}
+
+function paketBaru() {
+    const namaPengirim = document.getElementById('nama-Pengirim').value
+    const alamatPengirim = document.getElementById('alamat-Pengirim').value
+    const kotaAsal = document.getElementById('asal').value
+    const noHPPengirim = document.getElementById('noHP-Pengirim').value
+    const namaPenerima = document.getElementById('nama-penerima').value
+    const alamatPenerima = document.getElementById('alamat-penerima').value
+    const kotaTujuan = document.getElementById('tujuan').value
+    const noHPPenerima = document.getElementById('noHP-penerima').value
+    const berat = document.getElementById('berat').value
+
+    const ekonomi = document.getElementById('tipe-ekonomi').checked
+    const ekspress = document.getElementById('tipe-ekspress').checked
+
+    if (namaPengirim.length < 4) {
+        alert(`Nama Pengirim Minimal 4 Karakter`)
+        return
+    }
+    else if (noHPPengirim.length < 3) {
+        alert(`No HP Pengirim Minimal 3 Karakter`)
+        return
+    }
+
+    // console.log(ekonomi);
+    // console.log(ekspress);
+    // console.log(berat);
+
+    // console.log(namaPengirim);
+    // console.log(alamatPengirim);
+    // console.log(noHPPengirim);
+    // console.log(namaPenerima);
+    // console.log(alamatPenerima);
+    // console.log(noHPPenerima);
+
+    let objBaru = {
+        id : `${namaPengirim[0]}${namaPengirim[1]}${namaPengirim[2]}${namaPengirim[3]}${noHPPengirim[noHPPengirim.length - 3]}${noHPPengirim[noHPPengirim.length - 2]}${noHPPengirim[noHPPengirim.length - 1]}`,
+        namaPengirim : namaPengirim,
+        noHpPengirim : noHPPengirim,
+        alamatPengirim : alamatPengirim,
+        // kotaAsal : kamusKota[Math.floor(Math.random() * kamusKota.length)],
+        kotaAsal : kotaAsal,
+        namaPenerima : namaPenerima,
+        noHpPenerima : noHPPenerima,
+        alamatPenerima : alamatPenerima,
+        // kotaTujuan : kamusKota[Math.floor(Math.random() * kamusKota.length)],
+        kotaTujuan : kotaTujuan,
+        biaya : 10000,
+        status: `Sedang Dalam Pengiriman`,
+        updated : false
+    }
+
+    if (ekonomi) {
+        objBaru.biaya = 5000 * Number(Math.abs(berat)) 
+    }
+    else if (ekspress) {
+        objBaru.biaya = 10000 * Number(Math.abs(berat))
+    }
+
+    array.push(objBaru)
+
+    generateTable(array)
+
+    document.getElementById('nama-Pengirim').value = ''
+    document.getElementById('alamat-Pengirim').value = ''
+    document.getElementById('noHP-Pengirim').value = ''
+    document.getElementById('nama-penerima').value = ''
+    document.getElementById('alamat-penerima').value = ''
+    document.getElementById('noHP-penerima').value = ''
+    document.getElementById('tipe-ekonomi').checked = false
+    document.getElementById('tipe-ekspress').checked = false
+    document.getElementById('berat').value = ''
+}
+
+
+generateTable(array)
+
---
+++ readme.md
@@ -0,0 +1,51 @@
+PIC: Rizal
+
+IDE: Bikin 
+Tema: DELIVERY
+Link Repo: 
+Link Github.io: 
+
+Fitur Tambahan (optional):
+ex: Gatau
+- <fill>
+
+Task: 
+Rizal: Bikin Form
+Rendhy: Landing Page
+Azfar: Contac Us / Saran
+Handi: Bikin Tabel
+
+
+Program Delivery
+
+CRUD (Create, Read, Update, Delete)
+
+1. Landing Page => Informasi / daftar harga / tipe pengiriman
+
+2. Create Program => satu halaman (Order Now!)  && (Read / Update) Program => satu halaman (Status Barang)
+    -Buat form
+    Isi :
+        1. Nama Pengirim => langsung ambil 
+        2. Nama Penerima
+        3. Alamat Asal (Manipulasi string)
+        4. Alamat Tujuan (Manipulasi string)
+        5. NoHP Pengirim
+        6. NoHP Penerima
+        7. Jenis/Nama barang
+        8. Berat barang => (number)
+        9. Tipe pengiriman
+
+
+        10. Biaya Pengiriman => Output
+        11. Lama pengiriman => Output 
+
+
+    -Bikin Table 
+    No ||   ID    || Asal     || Tujuan   ||      Status                                || Action
+    1      A1234    Jakarta     Bandung        Sedang dikirim / Sampai Tujuan / Gagal       (Tombol : Delete) / (Tombol : Delete)
+    2      A1234    Jakarta     Bandung        Sedang dikirim / Sampai Tujuan / Gagal       (Tombol : Delete) / (Tombol : Delete)
+    3      A1234    Jakarta     Bandung        Sedang dikirim / Sampai Tujuan / Gagal       (Tombol : Delete) / (Tombol : Delete)
+    4      A1234    Jakarta     Bandung        Sedang dikirim / Sampai Tujuan / Gagal       (Tombol : Delete) / (Tombol : Delete)
+
+
+3. COntact us / saran
